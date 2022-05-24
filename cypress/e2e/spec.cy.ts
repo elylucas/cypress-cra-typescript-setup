d5997b2e1d004d38386a2b4fe3da9f56366663fa
Ely Lucas <ely@meta-tek.net>
2022-05-24 09:28:02 -0600

    init

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,3 @@
+it('abc', () => {
+  cy.mount('adsf')
+})
---
+++ src/App.cy.tsx
@@ -0,0 +1,12 @@
+// Import your component
+// import App from './App';
+
+import App from './App';
+
+
+
+describe('App.cy.tsx', () => {
+  it('playground', () => {
+    cy.mount(<App />)
+  });
+});
